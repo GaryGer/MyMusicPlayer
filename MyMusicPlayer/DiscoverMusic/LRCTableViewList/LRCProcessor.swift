7b10fa56a3de98829e8ebe5e273029cbd08f7721
GePengFei <18509297726@126.com>
2017-12-21 17:45:00 +0800

    load lrc and displayer

---
+++ MyMusicPlayer/DiscoverMusic/LRCTableViewList/LRCProcessor.swift
@@ -0,0 +1,13 @@
+//
+//  LRCProcessor.swift
+//  MyMusicPlayer
+//
+//  Created by Ger on 2017/12/21.
+//  Copyright © 2017年 GpfYxh.Ger.com. All rights reserved.
+//
+
+import UIKit
+
+class LRCProcessor: NSObject {
+
+}
